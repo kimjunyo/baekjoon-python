c1b39296a2f9f269a17d17d6501fb47736d6329d
김준영 <119035407+kimjunyo@users.noreply.github.com>
2025-02-23 22:23:14 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 2352 ms, Memory: 106860 KB -BaekjoonHub

---
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[][][] arr = new int[101][101][101];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        while (true) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            if (a == -1 && b == -1 && c == -1) {
+                return;
+            }
+
+            System.out.printf("w(%d, %d, %d) = %d\n", a, b, c, w(a, b, c));
+        }
+    }
+
+    private static int w(int a, int b, int c) {
+
+        if (a <= 0 || b <= 0 || c <= 0) {
+            return 1;
+        }
+        if (a > 20 || b > 20 || c > 20) {
+            return w(20, 20, 20);
+        }
+
+        if (arr[a + 50][b + 50][c + 50] != 0) {
+            return arr[a + 50][b + 50][c + 50];
+        }
+
+        if (a < b && b < c) {
+            arr[a + 50][b + 50][c + 50] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
+        } else {
+            arr[a + 50][b + 50][c + 50] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
+        }
+
+        return arr[a + 50][b + 50][c + 50];
+    }
+}

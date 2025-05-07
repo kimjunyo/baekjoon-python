c2b969776140d51e779dba9c4c76176fee15ef33
김준영 <119035407+kimjunyo@users.noreply.github.com>
2025-05-07 15:07:58 +0900

    [Gold IV] Title: 팰린드롬?, Time: 840 ms, Memory: 231584 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
@@ -2,35 +2,63 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+  static int[][] dp;
+  static int[] arr;
   public static void main(String[] args) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     int N = Integer.parseInt(br.readLine());
-    int[] arr = new int[N];
+    arr = new int[N+1];
+    dp = new int[N+1][N+1];
 
     StringTokenizer st = new StringTokenizer(br.readLine());
-    for(int i=0; i<N; i++) {
+    for(int i=1; i<N+1; i++) {
       arr[i] = Integer.parseInt(st.nextToken());
+    }
+
+    for(int i=1; i<=N; i++) {
+      Arrays.fill(dp[i], -1);
+      dp[i][i] = 1;
     }
 
     StringBuilder sb = new StringBuilder();
     int M = Integer.parseInt(br.readLine());
     for(int i=0; i<M; i++) {
       st = new StringTokenizer(br.readLine());
-      int start = Integer.parseInt(st.nextToken())-1;
-      int end = Integer.parseInt(st.nextToken())-1;
-      boolean isPalindrome = true;
+      int start = Integer.parseInt(st.nextToken());
+      int end = Integer.parseInt(st.nextToken());
       
-      while(start < end) {
-        if(arr[start++] != arr[end--]) {
-          isPalindrome = false;
-          break;
-        }
-      }
-      sb.append(isPalindrome ? 1 : 0).append("\n");
+      isDp(start, end);
+      sb.append(dp[start][end]).append("\n");
     }
     bw.write(sb.toString());
     bw.flush();
+  }
+
+  private static boolean isDp(int start, int end) {
+    if(dp[start][end] == -1) {
+      if(arr[start] != arr[end]) {
+        dp[start][end] = 0;
+        return false;
+      } else {
+        if(start >= end) {
+          dp[start][end] = 1;
+          return true;
+        }
+        if(isDp(start+1, end-1)) {
+          dp[start][end] = 1;
+          return true;
+        } else {
+          dp[start][end] = 0;
+          return false;
+        }
+      }
+    } else if (dp[start][end] == 0) {
+      return false;
+    } else {
+      return true;
+    }
+  
   }
 }
